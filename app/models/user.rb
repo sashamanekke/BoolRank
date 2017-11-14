8871b8267916568e7d3df153edece61ce1a620c9
sasha de laet <sasha.delaet@gmail.com>
2017-11-14 11:56:03 +0100

    create routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_one :profile
-  has_many :polls
-  has_many :participants
-  has_many :votes
+  has_one :profile, dependent: :destroy
+  has_many :polls, dependent: :destroy
+  has_many :participants, dependent: :destroy
+  has_many :votes, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,26 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+# DO I NEED THE ID
+# expose as little as possible
+  resources :polls do
+    resources :propositions
+    resources :votes, only: [:create]
+    member do
+      get "share_poll", to: 'poll#share_poll'
+      get "home", to: 'polls#home'
+      get "compare", to: 'polls#compare'
+      get "ask_rank", to: 'polls#ask_rank'
+      get "rank", to: 'polls#rank'
+      get "share_rank", to: 'polls#share_rank'
+    end
+    # collection do
+    #   get 'top-3', to: 'polls#top_3'
+    # end
+    resources :participants, only: [:index, :show, :create, :update, :destroy]
+  end
+  resources :profiles, only: [:show, :new, :create, :edit]
 end
