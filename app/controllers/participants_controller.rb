57bacb69455677cfcbcdc3a54c35a78ccf7deccf
Jun Joseph Padilla <jun.joseph.padilla@gmail.com>
2017-11-15 15:53:24 +0100

    participants_controller updated

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,10 +1,19 @@
 class ParticipantsController < ApplicationController
   def index
+    @participants = Participant.all
   end
 
   def create
+
   end
 
   def destroy
   end
+
+private
+
+# def set_participants
+#   @user = User.find(params[:user_id])
+# end
+
 end
