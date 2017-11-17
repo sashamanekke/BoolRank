722eea7ca393319287fbe4edae8d2a299bf8c8fe
sasha de laet <sasha.delaet@gmail.com>
2017-11-17 12:52:05 +0100

    added color attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171117090836) do
+ActiveRecord::Schema.define(version: 20171117114847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20171117090836) do
     t.bigint "poll_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "color"
     t.index ["poll_id"], name: "index_propositions_on_poll_id"
   end
 
