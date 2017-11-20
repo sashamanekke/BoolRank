14af22aed0ffef9b5d9415e2b26e3ba4d9d0cb91
Jun Joseph Padilla <jun.joseph.padilla@gmail.com>
2017-11-20 10:47:43 +0100

    added temp copy link on homepage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,5 +4,13 @@
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
 //= require attachinary
+//= require clipboard
 //= require_tree .
+
+$(document).ready(function(){
+
+  var clipboard = new Clipboard('.clipboard-btn');
+  console.log(clipboard);
+
+});
 
