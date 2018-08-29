3b954fbe8fbfa8fb07d8be24efa7be84d515aeea
Louise Picot <louisepicot@gmail.com>
2018-08-29 15:02:50 +0200

    iframe config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,9 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_dispatch.default_headers = {
+      'X-Frame-Options' => 'ALLOWALL'
+  }
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -45,9 +47,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
-config.action_dispatch.default_headers = {
-    'X-Frame-Options' => 'ALLOWALL'
-}
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
